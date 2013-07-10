5c38d148e00f241dbff896edbaab6fd8d121d72b
craig brookes <craigbrookes@gmail.com>
2013-07-10 23:43:08 +0100

    improved and filled out code added tests

--- README.md
+++ README.md
@@ -2,3 +2,22 @@ readline
 ========
 
 read line module for node js
+
+TODO
+=====
+
+test different new line chars \r
+
+Example
+=======
+
+```
+  var readline = require('readline');
+  var rl = readline("./somefile.txt");
+  rl.on("line", function (line){
+    //do something with the line of text
+  });
+  rl.on('error', function (e){
+    //something went wrong
+  });
+```
--- readline.js
+++ readline.js
@@ -1,23 +1,24 @@
 var fs = require('fs');
 var EventEmitter = require('events').EventEmitter;
 var util = require('util');
+
 function readLine (file, opts){
 
    if (!(this instanceof readLine)) return new readLine(file);;
   
    EventEmitter.call(this);
-   
+   opts = opts || {};
    var self = this;
    var readStream = fs.createReadStream(file);
-   var nl = "\n".charCodeAt(0);
+   var nlChar = (opts["nlChar"]) ? opts["nlChar"] : "\n";
+   var nl = nlChar.charCodeAt(0);
    var line = [];   
 
-   readStream.on("open",function (){
-        
+   readStream.on("open",function (fd){
+      self.emit('open',fd);  
    });
    
-   readStream.on("data", function (data){
-     console.error("data emmitted");
+   readStream.on("data", function (data){     
      for(var i=0; i < data.length; i++){
         if(data[i] === nl){
           var tmpBuf = new Buffer(line);
@@ -26,7 +27,6 @@ function readLine (file, opts){
         }else{
           line.push(data[i]); 
         }
-
      }
    });
    readStream.on("error", function (err){
@@ -35,10 +35,12 @@ function readLine (file, opts){
    });
 
    readStream.on("end", function (){
-
-     console.error("end of stream");
+    self.emit("end");
    });
    
+   readStream.on("close", function (){
+    self.emit("close");
+   });
 };
 
 util.inherits(readLine, EventEmitter);
