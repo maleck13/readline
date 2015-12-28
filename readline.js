e5a328add8e1522e0c4c25c40249b9f73430ac65
craig <cbrookes@redhat.com>
2015-12-28 15:22:22 +0000

    small tidy and name anon funcs

--- readline.js
+++ readline.js
@@ -14,7 +14,7 @@ var readLine = module.exports = function(file, opts) {
       lineLength = 0,
       lineCount = 0,
       byteCount = 0,
-      emit = function(lineCount, byteCount) {
+      emit = function spliceAndEmit(lineCount, byteCount) {
         try {
           var line = lineBuffer.slice(0, lineLength);
           self.emit('line', opts.retainBuffer? line : line.toString(), lineCount, byteCount);
@@ -25,11 +25,13 @@ var readLine = module.exports = function(file, opts) {
         }
       };
   this.input = ('string' === typeof file) ? fs.createReadStream(file, opts) : file;
-  this.input.on('open', function(fd) {
+  this.input
+    .on('open', function onOpen(fd) {
       self.emit('open', fd);
     })
-    .on('data', function(data) {
-      for (var i = 0; i < data.length; i++) {
+    .on('data', function onData(data) {
+      var dataLen = data.length;
+      for (var i = 0; i < dataLen; i++) {
         if (data[i] == 10 || data[i] == 13) { // Newline char was found.
           if (data[i] == 10) {
             lineCount++;
@@ -42,10 +44,10 @@ var readLine = module.exports = function(file, opts) {
         byteCount++;
       }
     })
-    .on('error', function(err) {
+    .on('error', function onError(err) {
       self.emit('error', err);
     })
-    .on('end', function() {
+    .on('end', function onEnd() {
       // Emit last line if anything left over since EOF won't trigger it.
       if (lineLength) {
         lineCount++;
@@ -53,7 +55,7 @@ var readLine = module.exports = function(file, opts) {
       }
       self.emit('end');
     })
-    .on('close', function() {
+    .on('close', function onClose() {
       self.emit('close');
     });
 };
